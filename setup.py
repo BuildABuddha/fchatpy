888e6cd034388fdc5980bdd102dc8a9e44847397
Lawrence Kimsey <34719668+BuildABuddha@users.noreply.github.com>
2020-11-23 15:12:53 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='FChatPy',
-    version='0.1',
+    version='0.2',
     author='Build-A-Buddha',
     python_requires='>=3',
     install_requires=['ws4py>=0.5.1'],
